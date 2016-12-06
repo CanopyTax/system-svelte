6f08f9810c556e8bbef1d2b8f80e1727ef6beb7e
Bret Little <bret.little@gmail.com>
2016-12-06 12:56:35 -0700

    Fix whitespace

--- svelte.js
+++ svelte.js
@@ -1,19 +1,19 @@
 var compile = require('svelte').compile;
 
 exports.translate = function(load) {
-    var output = compile(load.source, {
-      filename: load.address,
-      format: 'es',
-      onerror: (err) => {
-        console.log(err.message);
-        this.emitError(err.message);
-      },
-      onwarn: (warn) => {
-        console.log(warn.message);
-        this.emitWarn(warn.message);
-      }
-    });
+  var output = compile(load.source, {
+    filename: load.address,
+    format: 'es',
+    onerror: (err) => {
+      console.log(err.message);
+      this.emitError(err.message);
+    },
+    onwarn: (warn) => {
+      console.log(warn.message);
+      this.emitWarn(warn.message);
+    }
+  });
 
-		load.source = output.code;
-		load.metadata.sourceMap = output.map;
+  load.source = output.code;
+  load.metadata.sourceMap = output.map;
 }
